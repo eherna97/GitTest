940e29bd16349225ae7164418dc9e0a9499bd5fd
Eric Hernandez <eherna97@ucsc.edu>
2020-03-17 01:32:17 -0700

    Added echo

--- GitHubTest.java
+++ GitHubTest.java
@@ -1,7 +1,10 @@
-public class GitTest
+public class GitHubTest
 {
   public static void main(String[] args)
   {
     System.out.println("Hello!");
+
+    for(int i = 0; i < args.length; i++)
+      System.out.println(args[i]);
   }
 }
